f2db1844fb57a7113046e0f2b1ffd3798b196dd6
kalyan <116357692+amruthakalyan@users.noreply.github.com>
2024-06-02 13:01:32 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Joke from "./Components/Joke";
+
+function App() {
+	return (
+		<div className="App">
+			<h1>Jokes Generator Using React 
+        js and Joke API</h1>
+			<Joke/>
+		</div>
+	);
+}
+
+export default App;
