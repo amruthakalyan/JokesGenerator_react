32e63af2f98861836958f3814bca6ceb26b89865
kalyan <116357692+amruthakalyan@users.noreply.github.com>
2024-08-05 18:36:59 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Joke from "./Components/Joke";
-
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 function App() {
 	return (
 		<div className="App">
+			<Navbar/>
 			<h1>Jokes Generator Using React 
         js and Joke API</h1>
 			<Joke/>
+			<Footer/>
 		</div>
 	);
 }
